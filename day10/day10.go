54ab452383d9c8191a4224c996fd88a006dd0c1c
marcsek <jakubmarcek955@gmail.com>
2023-12-10 21:01:05 +0000

    commit

--- day10/day10.go
+++ day10/day10.go
@@ -25,9 +25,8 @@ var mvs = map[string][]int{
 }
 
 func main() {
-	input := strings.Split(readFile("sample.txt"), "\n")
+	input := strings.Split(readFile("test.txt"), "\n")
 	maze, s_y, s_x := genMaze(input)
-	maze_c, _, _ := genMaze(input)
 	traverse(s_y, s_x, 0, maze)
 
 	y, x := findEmpty(maze)
@@ -35,16 +34,7 @@ func main() {
 	for y != -1 && x != -1 {
 		found := [][]int{}
 		if findAreas(y, x, maze, &found) {
-			canSqueeze := false
-			for _, pc := range found {
-				y, x := pc[0], pc[1]
-				if trySqueezing(y, x, maze_c) {
-					canSqueeze = true
-				}
-			}
-			if !canSqueeze {
-				potCorrect = append(potCorrect, found...)
-			}
+			potCorrect = append(potCorrect, found...)
 		}
 		y, x = findEmpty(maze)
 	}
@@ -54,59 +44,15 @@ func main() {
 		maze[y][x] = "I"
 	}
 
-	fmt.Println(len(potCorrect))
-	for _, r := range maze {
-		for _, c := range r {
-			fmt.Print(c)
-		}
-		fmt.Println("")
-	}
-}
-
-func trySqueezing(y, x int, maze [][]string) bool {
-	around := getPipesAround(y, x, maze)
-
-	canSquueze := false
-	for i := 0; i < len(around)-1; i++ {
-		x1, y1 := around[i][1], around[i][0]
-		x2, y2 := around[i+1][1], around[i+1][0]
-		conn := chekIfConnected(x1, y1, x2, y2, maze)
-		if conn {
-			canSquueze = true
+	total := 0
+	for _, pc := range potCorrect {
+		y, x := pc[0], pc[1]
+		if y%2 == 0 && x%2 == 0 {
+			total += 1
 		}
 	}
-	if canSquueze {
-		fmt.Println(y, x)
-	}
-	return canSquueze
-}
-
-func getPipesAround(y, x int, maze [][]string) [][]int {
-	res := [][]int{}
-
-	for n_y := range []int{-1, 0, 1} {
-		for n_x := range []int{-1, 0, 1} {
-			if y+n_y < 0 || y+n_y >= len(maze) || x+n_x < 0 || x+n_x >= len(maze[0]) {
-				continue
-			}
-			p := maze[y+n_y][x+n_x]
-			if p != "." && p != "S" {
-				res = append(res, []int{y + n_y, x + n_x})
-			}
-		}
-	}
-	return res
-}
-
-func chekIfConnected(x1, y1, x2, y2 int, maze [][]string) bool {
-	p1, p2 := maze[y1][x1], maze[y2][x2]
-
-	for _, move := range pipes[p1] {
-		if move == rune(pipes[p2][0]) || move == rune(pipes[p2][1]) {
-			return true
-		}
-	}
-	return false
+	printActual(maze)
+	fmt.Println(total)
 }
 
 func findEmpty(maze [][]string) (int, int) {
@@ -124,7 +70,7 @@ func findAreas(y, x int, maze [][]string, found *[][]int) bool {
 	isInside := true
 	if y < 0 || y >= len(maze) || x < 0 || x >= len(maze[0]) {
 		return false
-	} else if maze[y][x] == "!" || maze[y][x] == "O" {
+	} else if maze[y][x] == "!" || maze[y][x] == "O" || maze[y][x] == "S" {
 		return true
 	}
 
@@ -198,13 +144,42 @@ func genMaze(input []string) ([][]string, int, int) {
 		row := []string{}
 		for i_x, c := range line {
 			if c == 'S' {
-				y, x = i_y, i_x
+				y, x = i_y*2, i_x*2
 			}
-			row = append(row, string(c))
+			toAdd := "."
+			if c == 'F' || c == 'L' || c == '-' || c == 'S' {
+				toAdd = "-"
+			}
+			row = append(row, string(c), toAdd)
 		}
-		maze = append(maze, row)
+		e_row := []string{}
+		for i := 0; i < len(row); i++ {
+			toAdd := "."
+			if row[i] == "|" || row[i] == "S" || row[i] == "7" || row[i] == "F" {
+				toAdd = "|"
+			}
+			e_row = append(e_row, toAdd)
+		}
+		maze = append(maze, row, e_row)
 	}
 	return maze, y, x
+}
+
+func printMaze(maze [][]string) {
+	for _, r := range maze {
+		for _, c := range r {
+			fmt.Print(c)
+		}
+		fmt.Println("")
+	}
+}
+func printActual(maze [][]string) {
+	for y := 0; y < len(maze); y += 2 {
+		for x := 0; x < len(maze[y]); x += 2 {
+			fmt.Print(maze[y][x])
+		}
+		fmt.Println("")
+	}
 }
 
 func readFile(fn string) string {
