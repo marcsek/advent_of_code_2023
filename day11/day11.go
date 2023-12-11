06417511a466a1c4501140bd3d85ace3d62003f0
marcsek <jakubmarcek955@gmail.com>
2023-12-11 11:40:21 +0000

    commit

--- day11/day11.go
+++ day11/day11.go
@@ -2,26 +2,48 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strings"
 )
 
-func main() {
-	input := strings.Split(readFile("sample.txt"), "\n")
-	maze := parseInput(input[:len(input)-1])
+const multBy = 1_000_000
 
-	fmt.Println(len(findEdges(maze)))
+func main() {
+	input := strings.Split(readFile("test.txt"), "\n")
+	expans := parseInput(input[:len(input)-1])
+	edges := findEdges(input)
+
+	fmt.Println(findLength(edges, expans))
 }
 
-func findLength(maze [][]rune) int {
-	for a1 := 0; a1 < len(maze); a1++ {
-		for a2 := a1 + 1; a2 < len(maze); a2++ {
+func findLength(edges [][]int, expans [][]int) int {
+	total := 0
+	for a1 := 0; a1 < len(edges); a1++ {
+		for a2 := a1 + 1; a2 < len(edges); a2++ {
+			c1, c2 := edges[a1], edges[a2]
+			y1, x1, y2, x2 := c1[0], c1[1], c2[0], c2[1]
 
+			mult := 0
+			for _, expan := range expans {
+				ey, ex := expan[0], expan[1]
+				y1, y2 := int(math.Min(float64(y1), float64(y2))), int(math.Max(float64(y1), float64(y2)))
+				x1, x2 := int(math.Min(float64(x1), float64(x2))), int(math.Max(float64(x1), float64(x2)))
+
+				if y1 < ey && y2 > ey {
+					mult += multBy - 1
+				}
+				if x1 < ex && x2 > ex {
+					mult += multBy - 1
+				}
+			}
+			total += int(math.Abs(float64((x1 - x2)))) + int(math.Abs(float64((y1 - y2)))) + mult
 		}
 	}
+	return total
 }
 
-func findEdges(maze [][]rune) [][]int {
+func findEdges(maze []string) [][]int {
 	edges := [][]int{}
 
 	for y, r := range maze {
@@ -34,42 +56,31 @@ func findEdges(maze [][]rune) [][]int {
 	return edges
 }
 
-func parseInput(input []string) [][]rune {
-	maze := [][]rune{}
-	for _, r := range input {
-		row := []rune{}
+func parseInput(input []string) [][]int {
+	expan := [][]int{}
+	for y, r := range input {
 		hasGalaxy := false
 		for _, s := range r {
 			if s == '#' {
 				hasGalaxy = true
 			}
-			row = append(row, s)
 		}
-		maze = append(maze, row)
 		if !hasGalaxy {
-			empty := []rune{}
-			for i := 0; i < len(row); i++ {
-				empty = append(empty, '.')
-			}
-			maze = append(maze, empty)
+			expan = append(expan, []int{y, 0})
 		}
 	}
-	correct := make([][]rune, len(maze))
-	for x := range maze[0] {
+	for x := range input[0] {
 		hasGalaxy := false
-		for i := 0; i < len(maze); i++ {
-			if maze[i][x] == '#' {
+		for y := range input {
+			if input[y][x] == '#' {
 				hasGalaxy = true
 			}
 		}
-		for y := range maze {
-			correct[y] = append(correct[y], maze[y][x])
-			if !hasGalaxy {
-				correct[y] = append(correct[y], '.')
-			}
+		if !hasGalaxy {
+			expan = append(expan, []int{0, x})
 		}
 	}
-	return correct
+	return expan
 }
 
 func printMaze(maze [][]rune) {
