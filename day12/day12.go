f64cfc6ebd3c2bb792ed2d54354958e6f82edcfe
Experrrt <jakubmarcek955@gmail.com>
2023-12-14 22:00:36 +0100

    commit

--- day12/day12.go
+++ day12/day12.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"slices"
 	"strconv"
@@ -10,140 +11,141 @@ import (
 	"gonum.org/v1/gonum/stat/combin"
 )
 
+// .??..??...?##.
+// .??..??...?##.?.??..??...?##. [1]
+
+// ?#?#?#?#?#?#?#?
+// ?#?#?#?#?#?#?#???#?#?#?#?#?#?#? [1]
+
+// ?????.#...#...
+
 type group struct {
-	Rec   []rune
-	Pos   []int
-	Quesm []int
+	rec   []rune
+	pos   []int
+	quesm []int
 }
 
-//???.###? ???.####
-//.??..??...?##. ?.??..??...?##.
-
 func main() {
-	input := strings.Split(readFile("test.txt"), "\n")
+	input := strings.Split(readfile("test.txt"), "\n")
 	groups := []group{}
 
 	for _, line := range input[:len(input)-1] {
 		sp := strings.Split(line, " ")
 
-		recNN := []rune{}
-		recN := []rune{}
-		if sp[0][len(sp[0])-1] != '#' {
-			recN = append(recN, '?')
-		}
-		//recN := []rune{'?'}
-		recN = append(recN, []rune(sp[0])...)
-		recNN = append(recNN, []rune(sp[0])...)
-		//recN = append(recN, '?')
-		if sp[0][len(sp[0])-1] != '#' {
-			if sp[0][len(sp[0])-1] != '?' {
-				rc := recNN
-				recNN = append([]rune{'?'}, rc...)
-			}
-			recN = getFiveMore(recN, 1)
-		} else {
-			recNN = append(recNN, '?')
-			recN = getFiveMore(recN, 1, '?')
-		}
-		recNNN := []rune{'?'}
-		if sp[0][len(sp[0])-1] == '#' {
-			recNNN = []rune{}
-		}
-		recNNN = append(recNNN, []rune(sp[0])...)
-		rec := getFiveMore([]rune(sp[0]), 1, '?')
+		rec := []rune{}
+		rec = append(rec, []rune(sp[0])...)
+		pos := strlisttoint(strings.Split(sp[1], ","))
 
-		posN := strListToInt(strings.Split(sp[1], ","))
-		if posN[len(posN)-1] == 1 && sp[0][len(sp[0])-1] == '?' {
-			recNNN = recNNN[1:]
-			recNNN = append(recNNN, '?')
-			rec = append(rec, '?')
-			recNN = append(recNN, '?')
-		}
-		pos := getFiveMore(posN, 1)
-		posN = getFiveMore(posN, 1)
+		prepMod := []rune{'?'}
+		appMod := []rune(sp[0])
+		appMod = append(appMod, '?')
+		prepMod = append(prepMod, []rune(sp[0])...)
+
 		quesm := []int{}
 		for x, r := range rec {
 			if r == '?' {
 				quesm = append(quesm, x)
 			}
 		}
-		groups = append(groups, group{Rec: []rune(rec), Pos: pos, Quesm: quesm})
-		quesm = []int{}
-		for x, r := range recN {
-			if r == '?' {
-				quesm = append(quesm, x)
-			}
-		}
-		groups = append(groups, group{Rec: []rune(recN), Pos: posN, Quesm: quesm})
-		quesm = []int{}
-		for x, r := range recNN {
-			if r == '?' {
-				quesm = append(quesm, x)
-			}
-		}
-		groups = append(groups, group{Rec: []rune(recNN), Pos: posN, Quesm: quesm})
+		groups = append(groups, group{rec: []rune(rec), pos: pos, quesm: quesm})
 
 		quesm = []int{}
-		for x, r := range recNNN {
+		for x, r := range prepMod {
 			if r == '?' {
 				quesm = append(quesm, x)
 			}
 		}
-		groups = append(groups, group{Rec: []rune(recNNN), Pos: pos, Quesm: quesm})
+		groups = append(groups, group{rec: []rune(prepMod), pos: pos, quesm: quesm})
+
+		quesm = []int{}
+		for x, r := range appMod {
+			if r == '?' {
+				quesm = append(quesm, x)
+			}
+		}
+		groups = append(groups, group{rec: []rune(appMod), pos: pos, quesm: quesm})
 	}
 
 	total := 0
-	for i := 0; i < len(groups); i += 4 {
-		d := genValid(groups[i+2])
-		p1 := genValid(groups[i+1])
-		p2 := genValid(groups[i])
-		p3 := d * d
-		p4 := genValid(groups[i+3])
-		//fmt.Println(p1 * p2 * p3 * p4)
-		total += p1 * p2 * p3 * p4
+	for i := 0; i < len(groups); i += 3 {
+		orgGroup := groups[i]
+		prepGroup := groups[i+1]
+		appGroup := groups[i+2]
+
+		// asi treba zdokonalit podmienku komplementu
+		validCompl := genvalid(appGroup)
+		canNegate := orgGroup.rec[len(orgGroup.rec)-1] == '#'
+
+		if potCompl := genvalid(prepGroup); potCompl > validCompl && !canNegate {
+			validCompl = potCompl
+		}
+		//fmt.Println(validCompl, orgGroup)
+
+		//fmt.Println(genvalid(orgGroup) * int(math.Pow(float64(genvalid(modGroup)), 4)))
+		//fmt.Println(
+		//	string(orgGroup.rec),
+		//	genvalid(orgGroup),
+		//	string(modGroup.rec),
+		//	genvalid(modGroup),
+		//	orgGroup.pos,
+		//	isChangable(string(orgGroup.rec), orgGroup.pos[0]),
+		//)
+
+		total += genvalid(orgGroup) * int(math.Pow(float64(validCompl), 4))
 	}
-	//fmt.Println(string(groups[12].Rec))
-	//fmt.Println(genValid(groups[12]))
-	//fmt.Println(string(groups[13].Rec))
-	//fmt.Println(genValid(groups[13]))
-	//fmt.Println(string(groups[14].Rec))
-	//fmt.Println(genValid(groups[14]))
-	//fmt.Println(string(groups[15].Rec))
-	//fmt.Println(genValid(groups[15]))
 	fmt.Println(total)
 }
 
-func getFiveMore[T any](sl []T, m int, z ...T) []T {
-	newSl := []T{}
-
-	for i := 0; i < m; i++ {
-		newSl = append(newSl, sl...)
-		if len(z) != 0 && i < m-1 {
-			newSl = append(newSl, z...)
-		}
+func isChangable(rec string, num int) bool {
+	total := 1
+	if rec[len(rec)-1] == '#' {
+		return false
 	}
-	return newSl
+
+	for _, r := range rec {
+		if r != '?' {
+			if r == '#' {
+				total--
+			}
+			break
+		}
+		total++
+	}
+
+	return num <= total
 }
 
-func genValid(g group) int {
-	left := sliceSum(g.Pos) - genAmount(g)
-	comb := combin.Combinations(len(g.Quesm), left)
+func getfivemore[t any](sl []t, m int, z ...t) []t {
+	newsl := []t{}
+
+	for i := 0; i < m; i++ {
+		newsl = append(newsl, sl...)
+		if len(z) != 0 && i < m-1 {
+			newsl = append(newsl, z...)
+		}
+	}
+	return newsl
+}
+
+func genvalid(g group) int {
+	left := slicesum(g.pos) - genamount(g)
+	comb := combin.Combinations(len(g.quesm), left)
 	c := 0
 
 	for _, p := range comb {
-		newStr := slices.Clone(g.Rec)
+		newstr := slices.Clone(g.rec)
 		for _, x := range p {
-			cor := g.Quesm[x]
-			newStr[cor] = '#'
+			cor := g.quesm[x]
+			newstr[cor] = '#'
 		}
-		if checkIfValid(newStr, g.Pos) {
+		if checkifvalid(newstr, g.pos) {
 			c++
 		}
 	}
 	return c
 }
 
-func sliceSum(s []int) int {
+func slicesum(s []int) int {
 	total := 0
 	for _, e := range s {
 		total += e
@@ -151,9 +153,9 @@ func sliceSum(s []int) int {
 	return total
 }
 
-func genAmount(g group) int {
+func genamount(g group) int {
 	am := 0
-	for _, r := range g.Rec {
+	for _, r := range g.rec {
 		if r == '#' {
 			am++
 		}
@@ -161,7 +163,7 @@ func genAmount(g group) int {
 	return am
 }
 
-func checkIfValid(rec []rune, pos []int) bool {
+func checkifvalid(rec []rune, pos []int) bool {
 	cur := 0
 	i := 0
 
@@ -182,7 +184,7 @@ func checkIfValid(rec []rune, pos []int) bool {
 	return true
 }
 
-func strListToInt(list []string) []int {
+func strlisttoint(list []string) []int {
 	c_list := []int{}
 	for i := 0; i < len(list); i++ {
 		if list[i] == "" {
@@ -190,18 +192,18 @@ func strListToInt(list []string) []int {
 		}
 		num, err := strconv.ParseInt(strings.TrimSpace(list[i]), 10, 0)
 		if err != nil {
-			panic("Couldn't parse int")
+			panic("couldn't parse int")
 		}
 		c_list = append(c_list, int(num))
 	}
 	return c_list
 }
 
-func readFile(fn string) string {
+func readfile(fn string) string {
 	data, err := os.ReadFile(fn)
 
 	if err != nil {
-		panic("Couldn't read input file")
+		panic("couldn't read input file")
 	}
 
 	return string(data)
